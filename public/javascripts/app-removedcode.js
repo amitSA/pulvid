269e8baa6623afe5931975f4786b0d1160f4d495
amitSA <send.amit@yahoo.com>
2016-12-11 21:20:41 -0800

    initial commit, made some modifications

---
+++ public/javascripts/app-removedcode.js
@@ -0,0 +1,22 @@
+﻿    // error handlers
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
